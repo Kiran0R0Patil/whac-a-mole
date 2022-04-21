ceec6ccd53d24cb2551de7eaefd3cb721000b537
Kiran R Patil <91089009+Kiran0R0Patil@users.noreply.github.com>
2022-04-21 11:56:43 +0530

    added start,level.restart js()

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const squares = document.querySelectorAll('.square')
 const mole = document.querySelector('.mole')
 const score = document.getElementById('score')
 const timeLeft = document.getElementById('time-left')
-const start = document.getElementById('start')
+const easy = document.querySelector('.easy')
+const medium = document.querySelector('.medium')
+const hard = document.querySelector('.hard')
+const starter = document.getElementById('starter')
 
 let result = 0;
 
-start.addEventListener('click', speed())
+easy.addEventListener('click', speedEasy)
+medium.addEventListener('click',speedMedium)
+hard.addEventListener('click', speedHard)
 
 function randomSquare(){
     squares.forEach(square => {
@@ -15,7 +20,20 @@ function randomSquare(){
     let randomSquare = squares[Math.floor(Math.random()*9)];
     randomSquare.classList.add('mole');
 }
-function speed(){
-    let timerId 
-    timerId = setInterval(randomSquare, 500)
+
+function speedEasy(){
+    starter.innerHTML = "<button onclick='setInterval(randomSquare, 500)'>START</button>";
+    reStart();
+}
+function speedMedium(){
+    starter.innerHTML = "<button onclick='setInterval(randomSquare, 350)'>START</button>";
+    reStart();
+}
+function speedHard(){
+    starter.innerHTML = "<button onclick='setInterval(randomSquare, 200)'>START</button>";
+    reStart();
+}
+
+function reStart(){
+    starter.innerHTML += "<button onclick='location.reload()'>RESTART</button>";
 }
