5f93154ec1d47fdd987d80020ce7a97d01feb2f9
Kiran R Patil <91089009+Kiran0R0Patil@users.noreply.github.com>
2022-04-21 19:24:39 +0530

    timer score update

--- app.js
+++ app.js
@@ -6,34 +6,72 @@ const easy = document.querySelector('.easy')
 const medium = document.querySelector('.medium')
 const hard = document.querySelector('.hard')
 const starter = document.getElementById('starter')
+const hits = document.getElementById('hits')
 
 let result = 0;
+let currentTime = 30;
+let hit =0;
+let timerId;
+let moleSpot;
+let countDownTimeIntervalId ;
 
-easy.addEventListener('click', speedEasy)
-medium.addEventListener('click',speedMedium)
-hard.addEventListener('click', speedHard)
-
+// randomly position the mole
 function randomSquare(){
     squares.forEach(square => {
         square.classList.remove('mole')
     })
     let randomSquare = squares[Math.floor(Math.random()*9)];
     randomSquare.classList.add('mole');
+    moleSpot = randomSquare.id
 }
 
+// to check if the player hit the mole
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id === moleSpot){
+            result++;
+            hit++;
+            score.textContent = result;
+            moleSpot = null;
+        }
+        else {
+            hit++;
+            moleSpot = null;
+        }
+    })
+})
+
+easy.addEventListener('click', speedEasy)
+medium.addEventListener('click',speedMedium)
+hard.addEventListener('click', speedHard)
+
+// give different speeds according to the level
 function speedEasy(){
-    starter.innerHTML = "<button onclick='setInterval(randomSquare, 500)'>START</button>";
-    reStart();
+    starter.innerHTML = "<button onclick='moveMole(700)'>START</button>";
 }
 function speedMedium(){
-    starter.innerHTML = "<button onclick='setInterval(randomSquare, 350)'>START</button>";
-    reStart();
+    starter.innerHTML = "<button onclick='moveMole(350)'>START</button>";
 }
 function speedHard(){
-    starter.innerHTML = "<button onclick='setInterval(randomSquare, 200)'>START</button>";
-    reStart();
+    starter.innerHTML = "<button onclick='moveMole(200)'>START</button>";
 }
 
-function reStart(){
-    starter.innerHTML += "<button onclick='location.reload()'>RESTART</button>";
+// on click start
+function moveMole(time){
+    timerId = setInterval(randomSquare, time)
+    countDownTimeIntervalId = setInterval(countDown, 1000)
+    // restart game
+    starter.innerHTML = "<button onclick='location.reload()'>RESTART</button>"; 
 }
+
+// countdown of the game
+function countDown(){
+ currentTime--; 
+ timeLeft.textContent = currentTime;
+ if(currentTime === 0){
+     clearInterval(countDownTimeIntervalId)
+     clearInterval(timerId)
+     hits.innerText = hit;
+ }
+}
+
